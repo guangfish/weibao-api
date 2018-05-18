9e14e574a9f78e9c02441e732ea21f8943825cc4
aineness <aineness@163.com>
2018-05-18 09:34:47 +0800

    2

--- weibao-api/src/main/java/com/new4s/weibao/api/WeibaoApiBooter.java
+++ weibao-api/src/main/java/com/new4s/weibao/api/WeibaoApiBooter.java
@@ -2,7 +2,7 @@ package com.new4s.weibao.api;
 
 import com.new4s.weibao.api.server.EmbbedJetty;
 
-public class WeibaoBooter {
+public class WeibaoApiBooter {
 
 	public static void main(String[] args) {
 		EmbbedJetty.main(args);
